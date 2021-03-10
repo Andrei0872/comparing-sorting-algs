d99289942a8c2d1905a154c167e0310196877bc1
Andrei Gatej <andreigtj01@gmail.com>
2021-03-11 00:26:15 +0200

    feat: add `bubble` sort

--- algs/bubble.go
+++ algs/bubble.go
@@ -1,5 +1,23 @@
 package algs
 
 func BubbleSort(arr []int) []int {
-	return []int{}
+	isSorted := false
+	arrLen := len(arr)
+
+	for !isSorted {
+		i := 0
+		isSorted = true
+
+		for ; i < arrLen-1; i++ {
+			if arr[i] > arr[i+1] {
+				temp := arr[i+1]
+				arr[i+1] = arr[i]
+				arr[i] = temp
+
+				isSorted = false
+			}
+		}
+	}
+
+	return arr
 }
