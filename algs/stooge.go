04c8e631351ec8e00de0bccda64d1acc992dbac8
Andrei Gatej <andreigtj01@gmail.com>
2021-03-13 00:41:41 +0200

    feat: add `Stooge Sort` algorithm

--- algs/stooge.go
+++ algs/stooge.go
@@ -1,3 +1,33 @@
 package algs
 
-func StoogeSort() {}
+func stoogeSort(a []int, firstIdx, lastIdx int) {
+	if firstIdx >= lastIdx {
+		return
+	}
+
+	if a[firstIdx] > a[lastIdx] {
+		a[firstIdx], a[lastIdx] = a[lastIdx], a[firstIdx]
+	}
+
+	sliceLen := lastIdx - firstIdx + 1
+
+	if !(sliceLen > 2) {
+		return
+	}
+
+	fraction := sliceLen / 3
+
+	// first 2/3 elements
+	stoogeSort(a, firstIdx, lastIdx-fraction)
+	// last 2/3 elements
+	stoogeSort(a, firstIdx+fraction, lastIdx)
+
+	// making sure the elements stay sorted after sorting the last 2/3 elements
+	stoogeSort(a, firstIdx, lastIdx-fraction)
+}
+
+func StoogeSort(a []int) []int {
+	stoogeSort(a, 0, len(a)-1)
+
+	return a
+}
